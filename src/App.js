da61f03b400c8c292baabc057c21a07baa6d5c59
Divyansh yadav <123111743+Divyansh21082005@users.noreply.github.com>
2025-05-08 14:10:17 +0530

    Social Media

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Calculator from './Calculator';
-
-
 
 function App() {
   return (
     <div className="App">
-      
-    <a>
-         Calculator
-          <Calculator/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-     
+      </header>
     </div>
   );
 }
