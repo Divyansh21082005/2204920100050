b4ee758f67065938fbad0295c0a681774ccd54aa
Divyansh yadav <123111743+Divyansh21082005@users.noreply.github.com>
2025-05-08 14:34:22 +0530

    Add files via upload

---
+++ Calculator/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+import Calculator from './Calculator';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    <a>
+         Calculator
+          <Calculator/>
+        </a>
+     
+    </div>
+  );
+}
+
+export default App;
